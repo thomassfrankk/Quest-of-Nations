81829e3843ab405cb2d9de8ecaee6a5be8a3d9d6
Thomas Frank <t.ostlyng@outlook.com>
2025-11-19 22:12:13 +0100

    Initial Commit

---
+++ Country Quiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country Quiz
+//
+//  Created by Thomas Frank on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country Quiz/Country_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_QuizApp.swift
+//  Country Quiz
+//
+//  Created by Thomas Frank on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Country_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
